eebd809f31e7b72460f5ec2916f181bf9e16cf2a
ysh <happysh_s2@naver.com>
2023-05-05 15:25:37 +0900

    Init study matching project :)

---
+++ StudyMatching/Source/Feature/Main/ViewController/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StudyMatching
+//
+//  Created by 양승현 on 2023/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemBlue
+  }
+
+}
---
+++ StudyMatching/Source/Support/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  StudyMatching
+//
+//  Created by 양승현 on 2023/05/05.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
+  }
+}
