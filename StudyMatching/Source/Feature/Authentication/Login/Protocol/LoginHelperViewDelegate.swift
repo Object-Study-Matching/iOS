63d35dbbdc2e420278770d9845e58eb5b5d5fbbd
ysh <happysh_s2@naver.com>
2023-06-06 05:21:08 +0900

    [Feat] LoginHelperView 에서 발생하는 이벤트 delegate 정의

---
+++ StudyMatching/Source/Feature/Authentication/Login/Protocol/LoginHelperViewDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  LoginHelperViewDelegate.swift
+//  StudyMatching
+//
+//  Created by 양승현 on 2023/06/06.
+//
+
+import Foundation
+
+protocol LoginHelperViewDelegate: AnyObject {
+  func gotoFindPassword()
+  func gotoFindId()
+  func gotoSignUp()
+}
