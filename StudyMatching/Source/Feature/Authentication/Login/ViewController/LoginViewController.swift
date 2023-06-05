d90b1f9fc80b0d1269705e7142bafdc2365e5eb5
ysh <happysh_s2@naver.com>
2023-06-06 05:22:02 +0900

    [Feat] LoginView 추가

--- StudyMatching/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
+++ StudyMatching/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 final class LoginViewController: UIViewController {
   // MARK: - Properties
+  private let loginView = LoginView()
   
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    loginView.setLayout(from: view)
+    view.backgroundColor = .white
   }
 }
