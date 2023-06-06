a8770d5fb9cc230050aa7350a8d9011453c7df69
이치훈 <dlclgns152615@gmail.com>
2023-06-06 22:42:06 +0900

    [Feat] StudyMatchingPostCell의 데이터를 구성할 Model입니다.

--- StudyMatching/Source/Feature/Home/ViewController/HomeViewController.swift
+++ StudyMatching/Source/Feature/Home/ViewController/HomeViewController.swift
@@ -36,6 +36,22 @@ class HomeViewController: UITableViewController {
         $0.setImage(UIImage(named: "addGroupButton"), for: .normal)
         $0.addTarget(self, action: #selector(showGroupSettingPage), for: .touchUpInside)
     }
+    
+    var studyMatchingPost = [
+        StudyMatchingPostModel(profileImage: UIImage(named: "2fb3451f2790627112ebba6732cb7a49"),
+                               userName: "양승현",
+                               mainTitle: "대전대에서 swift 스터디 하실분 구해요!",
+                               subscrive: "맥북만 있으면 모두 참가하실 수 있습니다."),
+        StudyMatchingPostModel(profileImage: UIImage(named: "2fb3451f2790627112ebba6732cb7a49"),
+                               userName: "김석현",
+                               mainTitle: "일렉기타 함께 배우실분??",
+                               subscrive: "함께 연주해봐요!"),
+        StudyMatchingPostModel(profileImage: UIImage(named: "2e88faead86a9f0d3bc1009b2a11bff5"),
+                               userName: "이치훈",
+                               mainTitle: "함께 공모전 참가할 팀원 구해요",
+                               subscrive: "K-해커톤, 함께할 디자이너, 백엔드 개발자 모십니다.")
+    ]
+    
     // let locationSelector = UIButton()
     
     // 버튼추가TODO: 이 기능은 다른 버튼으로 대체될 수 있음 (ex: 우측상단 슬라이드 버튼)
@@ -49,6 +65,14 @@ class HomeViewController: UITableViewController {
         searchTextField.delegate = self
         setupUI()
         navigationController?.setNavigationBarHidden(true, animated: false)
+        tableView.separatorStyle = .none
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        tableView.register(StudyMatchingPostCell.self, forCellReuseIdentifier: "StudyMatchingPostCell")
+        self.tableView.reloadData()
     }
     
 }
@@ -62,6 +86,44 @@ extension HomeViewController {
         print("push showGroupSettingPage")
         
         // present기능추가TODO: showGroupSettingPage
+        
+    }
+    
+}
+
+// MARK: - UITableViewLogic
+
+extension HomeViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studyMatchingPost.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "StudyMatchingPostCell", for: indexPath)
+            as? StudyMatchingPostCell {
+            
+            cell.profileImageView.image = studyMatchingPost[indexPath.row].profileImage
+            cell.userName.text = studyMatchingPost[indexPath.row].userName
+            cell.postedAt.text = "방금"
+            
+            cell.mainTitle.text = studyMatchingPost[indexPath.row].mainTitle
+            cell.subscrive.text = studyMatchingPost[indexPath.row].subscrive
+            
+            return cell
+        } else {
+            return StudyMatchingPostCell(style: .default, reuseIdentifier: "StudyMatchingPostCell")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // navigationPopTODO: - Group 모집 페이지로 이동
         
     }
     
---
+++ StudyMatching/Source/Feature/Home/ViewController/StudyMatchingPostCell.swift
@@ -0,0 +1,176 @@
+//
+//  StudyMatchingPostCell.swift
+//  StudyMatching
+//
+//  Created by 이치훈 on 2023/06/05.
+//
+
+import SnapKit
+import UIKit
+
+class StudyMatchingPostCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    let profileContainerView = UIView()
+    lazy var profileImageView = UIImageView().set {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 22
+    }
+    let userName: UILabel = UILabel().set {
+        $0.font = .boldSystemFont(ofSize: 13)
+    }
+    let postedAt: UILabel = UILabel().set {
+        $0.font = .systemFont(ofSize: 10)
+        $0.textColor = .lightGray
+    }
+    
+    let titleContainerView = UIView()
+    let mainTitle: UILabel = UILabel().set {
+        $0.font = .boldSystemFont(ofSize: 15)
+    }
+    let subscrive: UILabel = UILabel().set {
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .lightGray
+    }
+    
+    let buttonStackView: UIStackView = UIStackView().set {
+        $0.axis = .horizontal
+        $0.alignment = .fill
+        $0.distribution = .fillEqually
+    }
+    lazy var likeButton: UIButton = UIButton().set {
+        var attrStr = NSMutableAttributedString(string: "좋아요")
+        let attributes: [NSAttributedString.Key: Any] = [
+              .foregroundColor: UIColor.lightGray,
+              .font: UIFont.boldSystemFont(ofSize: 15)]
+        attrStr.addAttributes(
+          attributes,
+          range: NSRange(location: 0, length: "좋아용".count))
+        $0.setAttributedTitle(attrStr, for: .normal)
+        
+        $0.setImage(UIImage(systemName: "heart"), for: .normal)
+        $0.tintColor = .lightGray
+    }
+    lazy var commentButton: UIButton = UIButton().set {
+        var attrStr = NSMutableAttributedString(string: "댓글")
+        let attributes: [NSAttributedString.Key: Any] = [
+              .foregroundColor: UIColor.lightGray,
+              .font: UIFont.boldSystemFont(ofSize: 15)]
+        attrStr.addAttributes(
+          attributes,
+          range: NSRange(location: 0, length: "댓글".count))
+        $0.setAttributedTitle(attrStr, for: .normal)
+        
+        $0.setImage(UIImage(systemName: "bubble.left"), for: .normal)
+        $0.tintColor = .lightGray
+    }
+    
+    // MARK: - LifeCycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+    // postedAt를 UX를 위해 string으로 가공하는 func
+    func studyPostedAt(at: Date) -> String {
+        let currentDate = Date()
+        let time = Int(currentDate.timeIntervalSince(at) / 60)
+        
+        if time >= 1 {
+            return "\(time)분전"
+        } else {
+            return "방금"
+        }
+    }
+    
+}
+
+// MARK: - LayoutSupport Protocol
+
+extension StudyMatchingPostCell: LayoutSupport {
+    func addSubviews() {
+        self.addSubview(profileContainerView)
+        profileContainerView.addSubview(profileImageView)
+        profileContainerView.addSubview(userName)
+        profileContainerView.addSubview(postedAt)
+        self.addSubview(titleContainerView)
+        titleContainerView.addSubview(mainTitle)
+        titleContainerView.addSubview(subscrive)
+        self.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(likeButton)
+        buttonStackView.addArrangedSubview(commentButton)
+    }
+    
+    func setConstraints() {
+        
+        self.profileContainerView.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top)
+            make.leading.equalTo(self.snp.leading)
+            make.trailing.equalTo(self.snp.trailing)
+            make.bottom.equalTo(self.titleContainerView.snp.top)
+            make.height.equalTo(64)
+        }
+        
+        self.profileImageView.snp.makeConstraints { make in
+            make.leading.equalTo(self.snp.leading).offset(24)
+            make.trailing.equalTo(self.userName.snp.leading).offset(-5)
+            make.centerY.equalTo(self.profileContainerView.snp.centerY)
+            make.height.equalTo(44).priority(999)
+            make.width.equalTo(44).priority(999)
+        }
+        
+        self.userName.snp.makeConstraints { make in
+            make.top.equalTo(self.profileContainerView.snp.top).offset(15)
+            make.leading.equalTo(self.profileImageView.snp.trailing).offset(5)
+            make.bottom.equalTo(self.postedAt.snp.top).offset(-3)
+        }
+        
+        self.postedAt.snp.makeConstraints { make in
+            make.top.equalTo(self.userName.snp.bottom).offset(3)
+            make.leading.equalTo(self.profileImageView.snp.trailing).offset(5)
+        }
+        
+        self.titleContainerView.snp.makeConstraints { make in
+            make.top.equalTo(self.profileContainerView.snp.bottom)
+            make.leading.equalTo(self.snp.leading)
+            make.trailing.equalTo(self.snp.trailing)
+            make.bottom.equalTo(self.buttonStackView.snp.top)
+            make.height.equalTo(85)
+        }
+        
+        self.mainTitle.snp.makeConstraints { make in
+            make.top.equalTo(self.titleContainerView.snp.top)
+            make.leading.equalTo(self.titleContainerView.snp.leading).offset(24)
+            make.trailing.equalTo(self.titleContainerView.snp.trailing).offset(-24)
+            make.bottom.equalTo(self.subscrive.snp.top).offset(-10)
+        }
+        
+        self.subscrive.snp.makeConstraints { make in
+            make.top.equalTo(self.mainTitle.snp.bottom).offset(10)
+            make.leading.equalTo(self.titleContainerView.snp.leading).offset(24)
+            make.trailing.equalTo(self.titleContainerView.snp.trailing).offset(-24)
+        }
+        
+        self.buttonStackView.snp.makeConstraints { make in
+            make.top.equalTo(titleContainerView.snp.bottom)
+            make.leading.equalTo(self.snp.leading)
+            make.trailing.equalTo(self.snp.trailing)
+            make.bottom.equalTo(self.snp.bottom)
+            make.height.equalTo(20)
+        }
+        
+    }
+
+}
