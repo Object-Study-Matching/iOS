33f321352f2a309cd8352e6b36926b3ebe81f113
seokhyun <letushyun@gmail.com>
2023-06-08 03:59:16 +0900

    [Feat] Chat tableView 추가, combine으로 vm 연결

--- StudyMatching/Source/Feature/Chat/ViewController/ChatViewController.swift
+++ StudyMatching/Source/Feature/Chat/ViewController/ChatViewController.swift
@@ -5,13 +5,171 @@
 //  Created by 이치훈 on 2023/06/04.
 //
 
+import Combine
 import UIKit
 
-class ChatViewController: UIViewController {
+import SnapKit
+
+final class ChatViewController: UIViewController {
+  // MARK: - Properties
+  private lazy var input = ChatViewModel.Input()
+  private let viewModel = ChatViewModel()
+  private var subscriptions = Set<AnyCancellable>()
+  private lazy var tableView: UITableView = .init().set {
+    $0.register(ChatCell.self, forCellReuseIdentifier: ChatCell.id)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    $0.dataSource = self
+    $0.delegate = self
+  }
+  
+  private let stackView: UIStackView = .init().set {
+    $0.axis = .horizontal
+    $0.spacing = 26
+    $0.distribution = .fillEqually
+  }
+  
+  private lazy var messageButton: UIButton = .init().set {
+    $0.setTitle("쪽지", for: .normal)
+    $0.titleLabel?.font = .systemFont(ofSize: 14)
+    $0.setTitleColor(.black, for: .normal)
+    $0.tintColor = .blue
+    $0.layer.cornerRadius = 12
+    $0.layer.borderWidth = 1
+    $0.layer.borderColor = UIColor.black.cgColor
+    
+    $0.addTarget(self, action: #selector(didTapMessageButton), for: .touchUpInside)
+  }
+  
+  private lazy var studyButton: UIButton = .init().set {
+    $0.setTitle("스터디", for: .normal)
+    $0.titleLabel?.font = .systemFont(ofSize: 14)
+    $0.setTitleColor(.black, for: .normal)
+    $0.tintColor = .blue
+    $0.layer.cornerRadius = 12
+    $0.layer.borderWidth = 1
+    $0.layer.borderColor = UIColor.black.cgColor
+    
+    $0.addTarget(self, action: #selector(didTapStudyButton), for: .touchUpInside)
+  }
+  
+  // MARK: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    setupUI()
+    bind()
+  }
+}
+
+// MARK: - Helpers
+extension ChatViewController {
+  private func bind() {
+    let output = viewModel.transform(input)
+    
+    output
+      .receive(on: RunLoop.main)
+      .sink { [weak self] result in
+        switch result {
+        case .finished:
+          print("finished")
+        case let .failure(error):
+          self?.handleError(error)
+        }
+      } receiveValue: { [weak self] in
+        self?.render($0)
+      }
+      .store(in: &subscriptions)
+  }
+  
+  private func render(_ state: ChatViewModel.State) {
+    // outputTODO: - output logic
+    switch state {
+    case .changeToMessageCell:
+      print("쪽찌 section으로 바꿈")
+    case .changeToStudyCell:
+      print("스터디 section로 바꿈")
+    case .gotoMessageVC:
+      print("쪽지 VC로 화면전환")
+    }
+  }
+  
+  private func handleError(_ error: ChatViewModel.ViewModelError) {
+    switch error {
+    case .none:
+      print("DEBUG: Error not occured")
+    case .fatalError:
+      print("DEBUG: Fatal Error occured")
+    case .unexpected:
+      print("DEBUG: Unexpected error occured")
+    }
+  }
+}
+
+// MARK: - Actions
+extension ChatViewController {
+  @objc private func didTapMessageButton() {
+    input.didTapMessageButton.send()
+  }
+  
+  @objc private func didTapStudyButton() {
+    input.didTapStudyButton.send()
+  }
+}
+
+// MARK: - LayoutSupport
+extension ChatViewController: LayoutSupport {
+  func addSubviews() {
+    view.addSubview(stackView)
+    _ = [messageButton, studyButton].map { stackView.addArrangedSubview($0) }
+    view.addSubview(tableView)
+  }
+  
+  func setConstraints() {
+    stackView.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.leading.trailing.equalToSuperview().inset(42)
+      $0.height.equalTo(42)
     }
     
+    tableView.snp.makeConstraints {
+      $0.top.equalTo(stackView.snp.bottom)
+      $0.bottom.equalTo(view.safeAreaLayoutGuide)
+      $0.leading.trailing.equalToSuperview().inset(16)
+    }
+  }
+}
+
+// MARK: - UITableViewDataSource
+extension ChatViewController: UITableViewDataSource {
+  func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection section: Int
+  ) -> Int {
+    return ChatModel.models.count
+  }
+  
+  func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatCell.id, for: indexPath)
+    as? ChatCell else { return UITableViewCell() }
+    
+    let model = ChatModel.models[indexPath.row]
+    cell.configure(model)
+    return cell
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension ChatViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 60
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true) // selected cell color 제거
+    
+    input.didSelectedCell.send()
+  }
 }
--- StudyMatching/Source/Feature/Chat/ViewModel/ChatViewModel.swift
+++ StudyMatching/Source/Feature/Chat/ViewModel/ChatViewModel.swift
@@ -5,4 +5,71 @@
 //  Created by SeokHyun on 2023/06/08.
 //
 
+import Combine
 import Foundation
+
+final class ChatViewModel {
+//  typealias Input = ChatViewModel.Input
+//  typealias State = ChatViewModel.State
+//  typealias ViewModelError = ChatViewModel.ViewModelError
+  typealias Output = AnyPublisher<State, ViewModelError>
+  struct Input {
+    let didTapMessageButton: PassthroughSubject<Void, ViewModelError>
+    let didTapStudyButton: PassthroughSubject<Void, ViewModelError>
+    let didSelectedCell: PassthroughSubject<Void, Never>
+    
+    init(
+      didTapMessageButton: PassthroughSubject<Void, ViewModelError> = .init(),
+      didTapStudyButton: PassthroughSubject<Void, ViewModelError> = .init(),
+      didSelectedCell: PassthroughSubject<Void, Never> = .init()
+    ) {
+      self.didTapMessageButton = didTapMessageButton
+      self.didTapStudyButton = didTapStudyButton
+      self.didSelectedCell = didSelectedCell
+    }
+  }
+  
+  enum State {
+    case changeToMessageCell
+    case changeToStudyCell
+    case gotoMessageVC
+  }
+  
+  enum ViewModelError: Error {
+    case none
+    case fatalError
+    case unexpected
+  }
+}
+// MARK: - Transform
+extension ChatViewModel {
+  func transform(_ input: Input) -> Output {
+    return Publishers.MergeMany(
+      didTapMessageButtonStream(input),
+      didTapStudyButtonStream(input),
+      didSelectedCellStream(input)
+    )
+    .eraseToAnyPublisher()
+  }
+  
+  private func didTapMessageButtonStream(_ input: Input) -> Output {
+    return input.didTapMessageButton
+      .tryMap { State.changeToMessageCell }
+      .mapError { $0 as? ViewModelError ?? .unexpected }
+      .eraseToAnyPublisher()
+  }
+  
+  private func didTapStudyButtonStream(_ input: Input) -> Output {
+    return input.didTapStudyButton
+      .tryMap { State.changeToStudyCell }
+      .mapError { $0 as? ViewModelError ?? .unexpected }
+      .eraseToAnyPublisher()
+  }
+  
+  private func didSelectedCellStream(_ input: Input) -> Output {
+    return input.didSelectedCell
+      .map { State.gotoMessageVC }
+      .setFailureType(to: ViewModelError.self)
+      .eraseToAnyPublisher()
+  }
+}
