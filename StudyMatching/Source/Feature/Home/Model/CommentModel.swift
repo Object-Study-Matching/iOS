2f5b72e9e806531dd163e5ad49792824c7f90a6e
logicHoon <dlclgns152615@gmail.com>
2023-06-09 01:43:53 +0900

    [Feat] CommentCell에 댓글UI를 구현했습니다.

---
+++ StudyMatching/Source/Feature/Home/Model/CommentModel.swift
@@ -0,0 +1,17 @@
+//
+//  CommentModel.swift
+//  StudyMatching
+//
+//  Created by 이치훈 on 2023/06/08.
+//
+
+import UIKit
+
+struct CommentModel {
+  
+  var profileImage = UIImage(systemName: "person.crop.circle")
+  var userName = "UserName"
+  var comment = ""
+  var postedDate = Date()
+  
+}
--- StudyMatching/Source/Feature/Home/Model/StudyPostModel.swift
+++ StudyMatching/Source/Feature/Home/Model/StudyPostModel.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 struct StudyPostModel {
-    
-    var profileImage = UIImage(systemName: "person.crop.circle")
-    var userName = "UserName"
-    var postedAt = "방금"
-    var mainTitle = "Title"
-    var descrip = "description"
-    
-    var likeCount = 0
-    var commentCount = 0
-    
+  
+  var profileImage = UIImage(systemName: "person.crop.circle")
+  var userName = "UserName"
+  var postedAt = "방금"
+  var mainTitle = "Title"
+  var descrip = "description"
+  
+  var likeCount = 0
+  var commentCount = 0
+  
 }
