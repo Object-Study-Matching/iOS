3355aee1a61bb0115c7badadfb42bff5fc19c6c5
seokhyun <letushyun@gmail.com>
2023-06-09 04:49:36 +0900

    [Test] Mock 구조 변경

--- StudyMatching/Source/Feature/Chat/Model/ChatModel.swift
+++ StudyMatching/Source/Feature/Chat/Model/ChatModel.swift
@@ -1,5 +1,5 @@
 //
-//  ChatModel.swift
+//  ChatItemModel.swift
 //  StudyMatching
 //
 //  Created by SeokHyun on 2023/06/08.
@@ -7,36 +7,117 @@
 
 import Foundation
 
-struct ChatModel {
-  var userName: String
-  var description: String
-  var dateString: String
-  var profileImage: String
+struct ChatItemModel {
+  struct LetterModel {
+    var userName: String
+    var recentMessage: String
+    var recentDateString: String
+    var profileImage: String
+  }
+  
+  struct GroupChatModel {
+    var groupName: String
+    var recentDateString: String
+    var profileImage: String?
+    var recentMessage: String?
+    var memberCount: Int
+    var messageCount: Int
+  }
 }
-
-extension ChatModel {
-  static var models: [ChatModel] = [
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1"),
-    .init(
-      userName: "이치훈", description: "안녕하세요~ 글 보고 쪽지드려요!", dateString: "방금 전", profileImage: "userProfile1")
-    ]
+extension ChatItemModel {
+  static var letterModels: [LetterModel] = [
+    .init(userName: "이치훈이치훈이치훈이치훈이치훈이치훈이치훈이치훈이치훈이치훈이치훈이치훈이치훈이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1"),
+    .init(userName: "이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1"),
+    .init(userName: "이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1"),
+    .init(userName: "이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1"),
+    .init(userName: "이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1"),
+    .init(userName: "이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1"),
+    .init(userName: "이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1"),
+    .init(userName: "이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1"),
+    .init(userName: "이치훈",
+          recentMessage: "게시글 보고 쪽지 보냅니다. 게시글 보고 쪽지 보냅니다.",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1")
+  ]
+  
+  static var groupChatModels: [GroupChatModel] = [
+    .init(groupName: "토익 스터디 모임 토익 스터디 모임 토익 스터디 모임 토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile1",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123),
+    .init(groupName: "토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile2",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123),
+    .init(groupName: "토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile2",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123),
+    .init(groupName: "토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile2",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123),
+    .init(groupName: "토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile2",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123),
+    .init(groupName: "토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile2",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123),
+    .init(groupName: "토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile2",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123),
+    .init(groupName: "토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile2",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123),
+    .init(groupName: "토익 스터디 모임",
+          recentDateString: "방금 전",
+          profileImage: "userProfile2",
+          recentMessage: "저희 이번주 어디서 스터디 하나요? 저희 이번주 어디서 스터디 하나요?",
+          memberCount: 4,
+          messageCount: 123)
+  ]
 }
